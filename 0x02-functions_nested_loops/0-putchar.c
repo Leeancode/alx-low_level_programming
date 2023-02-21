125609002aab01d839ae40c6eb079a017c90f342
Leeancode <pydevop01@gmail.com>
2023-02-21 15:45:57 +0100

    Printing strings using putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - prints a string using the putchar function.
+ *
+ * Return: return (0) success.
+*#define LEEANCODE 9
+*int main(void)
+*{ char message[] = "_putchar";
+*	for (int i = 0; i < LEEANCODE; i++)
+*	{
+*		if (message[i] == '\0')
+*		{
+*			putchar('\n');
+*			break;
+*		}
+*		else
+*			putchar(message[i]);
+*	}
+*	return (0);
+*}
+*/
+
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+
+	int i, j;
+
+	/* j = sizeof(str) / sizeof(int);*/
+	j = 8;
+	for (i = 0; i < j; i++)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
