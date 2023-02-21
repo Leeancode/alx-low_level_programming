492804343f3f684a5e769bafa8245e8aa2c99ddd
Leeancode <pydevop01@gmail.com>
2023-02-21 23:31:54 +0100

    Adding main.h and modiied 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,37 +4,17 @@
  * main - prints a string using the putchar function.
  *
  * Return: return (0) success.
-*#define LEEANCODE 9
-*int main(void)
-*{ char message[] = "_putchar";
-*	for (int i = 0; i < LEEANCODE; i++)
-*	{
-*		if (message[i] == '\0')
-*		{
-*			putchar('\n');
-*			break;
-*		}
-*		else
-*			putchar(message[i]);
-*	}
-*	return (0);
-*}
 */
-
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-
-	int i, j;
-
-	/* j = sizeof(str) / sizeof(int);*/
-	j = 8;
-	for (i = 0; i < j; i++)
-	{
-		putchar(str[i]);
-	}
-
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	putchar('\n');
-
 	return (0);
 }
