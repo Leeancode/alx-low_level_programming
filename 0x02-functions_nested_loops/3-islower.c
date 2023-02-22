5ec5338a1bb5c66470ed323f19c2be99a5144679
Leeancode <pydevop01@gmail.com>
2023-02-22 02:14:24 +0100

    function to check if a character is lower or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_islower - function to check if a character is lower or upper case.
+*@c: The character to be checked.
+* Return: 0 success.
+*
+*/
+int _islower(int c)
+	if (c >= 'a' && c <= 'z')
+{ return (1);
+	else
+		return (0);
+}
