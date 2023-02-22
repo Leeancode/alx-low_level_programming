1cb7d7261f8d74989a40a9c8fdaebac1129e6986
Leeancode <pydevop01@gmail.com>
2023-02-22 03:06:57 +0100

    Function to print signs of input number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,14 @@ int print_sign(int n)
 		return (1);
 		_putchar(43);
 	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
 	}
 }
