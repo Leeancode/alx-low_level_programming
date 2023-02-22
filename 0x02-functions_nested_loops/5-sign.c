37e1d887b1f378d0bed5b212e07915408078d2b7
Leeancode <pydevop01@gmail.com>
2023-02-22 03:02:03 +0100

    Function to print signs of input number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - function that prints sign of a number
+ *
+ * @n: number to be tested
+ *Return: 1 if greater 0 and 0 equal to 0 and -1 if less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar(43);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar("45");
+		return (-1);
+	}
+}
