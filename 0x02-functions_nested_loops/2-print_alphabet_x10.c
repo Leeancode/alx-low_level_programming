7130b744de3761871ea90232e17db0d1c006d2a6
Leeancode <pydevop01@gmail.com>
2023-02-22 01:31:31 +0100

    changed to for loop nested in while loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,9 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	int j;
+	int i = 0, j;
 
-	for (i = 0; i <= 9; i++)
+	while (i < 10)
 	{
 		for (j = 97; j <= 122; j++)
 		{
@@ -18,4 +17,6 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
+
+	i++
 }
