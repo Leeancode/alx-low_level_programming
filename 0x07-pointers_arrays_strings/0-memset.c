bbcfa08cac834dda749dea601047e26f332ad2c8
Leeancode <pydevop01@gmail.com>
2023-03-08 03:22:08 +0100

    function that fills memoery with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* *_memset -fills memory with conctant byte
+*@s: pointer to constant b
+*@b: constant byte b
+*@n: bytes to use
+*Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; n--)
+	{
+		s[i] = b;
+	}
+return (s);
+}
